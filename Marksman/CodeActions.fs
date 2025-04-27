3358d96f983ba1d1ee9fa2ccd073e63439349aad
Benny Powers <web@bennypowers.com>
2025-04-27 13:38:27 +0300

    style: format

--- Marksman/CodeActions.fs
+++ Marksman/CodeActions.fs
@@ -225,12 +225,7 @@ let linkToReference (range: Range) (context: CodeActionContext) (doc: Doc) : Cod
             }
         | _ -> None
 
-    (* get the markdown link at the given range *)
-    doc.Index.mdLinks
-    |> Seq.tryFind (fun x ->
-        let range = Node.range x
-        range.Start <= range.Start && range.End >= range.End)
-    |> Option.bind (fun link ->
+    let getAction (link: Node<MdLink>) : CodeAction option =
         let linkDef =
             doc.Index.linkDefs
             |> Seq.tryFind (fun x ->
@@ -240,4 +235,12 @@ let linkToReference (range: Range) (context: CodeActionContext) (doc: Doc) : Cod
 
         match linkDef with
         | Some(def) -> getExistingRefAction (link, def)
-        | None -> getNonExistingRefAction link)
+        | None -> getNonExistingRefAction link
+
+    let isInRange (range: Range) token =
+        token.range.Start >= range.Start && token.range.End <= range.End
+
+    (* get the markdown link at the given range *)
+    doc.Index.mdLinks
+    |> Seq.tryFind (isInRange range)
+    |> Option.bind getAction
